42bd232a6f319c7e33c9c47006121075fcaccd07
stn <smrcouto88@gmail.com>
2023-06-02 20:14:27 -0300

    Icons

--- deskStructure.js
+++ deskStructure.js
@@ -1,34 +1,45 @@
+
+import { AiOutlineFileText } from "react-icons/ai";
+import { BsCalendarCheck } from "react-icons/bs";
+import { Gi3DMeeple, GiSpellBook, GiFruitTree } from "react-icons/gi";
+
 export const myStructure = (S) =>
   S.list()
     .title('Contenido')
     .items([
       S.listItem()
         .title('Institucional')
+        .icon(GiFruitTree)
         .child(
           S.list()
             .title('Institucional')
             .items([
               S.listItem()
                 .title('Quienes Somos')
+                .icon(AiOutlineFileText)
                 .child(S.document().schemaType('quienes_somos').documentId('quienes_somos')),
               S.listItem()
                 .title('Nuestra Historia')
+                .icon(AiOutlineFileText)
                 .child(S.document().schemaType('nuestra_historia').documentId('nuestra_historia')),
             ])
         ),
       S.divider(),
       S.listItem()
         .title('Calendario')
+        .icon(BsCalendarCheck)
         .schemaType('calendarioLudico')
         .child(S.documentTypeList('calendarioLudico').title('Calendario')),
       S.divider(),
       S.listItem()
         .title('Recursos')
+        .icon(Gi3DMeeple)
         .schemaType('recursos')
         .child(S.documentTypeList('recursos').title('Recursos')),
       S.divider(),
       S.listItem()
         .title('Que hacemos')
+        .icon(GiSpellBook)
         .schemaType('quehacemos')
         .child(S.documentTypeList('quehacemos').title('Que hacemos')),
       S.divider(),
